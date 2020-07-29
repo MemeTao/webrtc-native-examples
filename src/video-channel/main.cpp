b98ddd8a7d2305d26e937ad3c70489286e3e9a1a
陈涛 <tao.chen@shunwang.com>
2020-07-29 19:05:59 +0800

    sync code(video channel),day2

--- src/video-channel/main.cpp
+++ src/video-channel/main.cpp
@@ -14,6 +14,7 @@
 
 #include "media/base/video_adapter.h"
 #include "media/base/video_broadcaster.h"
+
 #include "pc/video_track_source.h"
 
 static auto g_signal_thread = rtc::Thread::CreateWithSocketServer();
@@ -83,20 +84,34 @@ class DummySetSessionDescriptionObserver
   virtual void OnFailure(webrtc::RTCError error) { assert(false);}
 };
 
+class VideoStreamReceiver : public rtc::VideoSinkInterface<webrtc::VideoFrame> {
+public:
+    void OnFrame(const webrtc::VideoFrame& frame) override {
+        std::cout<<"[info] received a frame, id:"<<frame.id()<<std::endl;
+    }
+};
 
-class SimpleClient : public webrtc::DataChannelObserver,
-    public webrtc::PeerConnectionObserver,
+class SimpleClient : public webrtc::PeerConnectionObserver,
     public webrtc::CreateSessionDescriptionObserver{
 public:
-    SimpleClient()
-        : peer_connection_(nullptr)
-    {
+    SimpleClient(bool sending) {
+        if(sending) {
+            video_receiver_ = std::make_shared<VideoStreamReceiver>();
+        }
     }
-    ~SimpleClient()
-    {
+    ~SimpleClient() {
+        if(peer_connection_) {
+            peer_connection_->Close();
+            peer_connection_ = nullptr;
+        }
     }
-    void start()
-    {
+    void bind_peerconnection(rtc::scoped_refptr<webrtc::PeerConnectionInterface> conn) {
+        peer_connection_ = conn;
+    }
+    void set_other(SimpleClient* receiver) {
+        other_ = receiver;
+    }
+    void start() {
         g_signal_thread->Invoke<void>(RTC_FROM_HERE, [this]()
         {
             peer_connection_->CreateOffer(
@@ -114,17 +129,29 @@ public:
         }
     }
 protected:
-    void OnIceCandidate(const webrtc::IceCandidateInterface* candidate) override
-    {
+    void OnAddStream(rtc::scoped_refptr<webrtc::MediaStreamInterface> stream) override{
+        std::cout<<"[info] on add stream, id:"<<stream->id()<<std::endl;
+    }
+    void OnAddTrack(rtc::scoped_refptr<webrtc::RtpReceiverInterface> receiver,
+            const std::vector<rtc::scoped_refptr<webrtc::MediaStreamInterface>>& streams) override {
+        auto track = receiver->track().get();
+        std::cout<<"[info] on add track,kind:"<<track->kind()<<std::endl;
+        if(track->kind() == "video" && video_receiver_) {
+            auto cast_track = static_cast<webrtc::VideoTrackSource*>(track);
+            cast_track->AddOrUpdateSink(video_receiver_.get(), rtc::VideoSinkWants());
+        }
+    }
+    void OnIceCandidate(const webrtc::IceCandidateInterface* candidate) override {
         peer_connection_->AddIceCandidate(candidate);
         std::string candidate_str;
         candidate->ToString(&candidate_str);
         /* sending ice to remote */
+        other_->on_ice_candidate(candidate);
     }
     //unused
-    void OnSignalingChange(
-          webrtc::PeerConnectionInterface::SignalingState new_state) override {}
+    void OnSignalingChange(webrtc::PeerConnectionInterface::SignalingState new_state) override {}
     void OnRenegotiationNeeded() override {}
+    void OnDataChannel(rtc::scoped_refptr<webrtc::DataChannelInterface> data_channel) override {}
     void OnIceGatheringChange(webrtc::PeerConnectionInterface::IceGatheringState new_state) override {}
 protected:
     virtual void OnSuccess(webrtc::SessionDescriptionInterface* desc) override {
@@ -133,6 +160,8 @@ protected:
         std::string sdp_str;
         desc->ToString(&sdp_str);
         /* sending sdp to remote */
+        auto sdp_cp = webrtc::CreateSessionDescription(desc->GetType(), sdp_str, nullptr);
+        other_->on_sdp(sdp_cp.release());
     }
     virtual void OnFailure(webrtc::RTCError error) {
         std::cout<<"[error] err:"<<error.message()<<std::endl;
@@ -140,20 +169,41 @@ protected:
     }
 
 private:
-    rtc::scoped_refptr<webrtc::PeerConnectionInterface> peer_connection_;
+    rtc::scoped_refptr<webrtc::PeerConnectionInterface> peer_connection_ = nullptr;
+    std::shared_ptr<VideoStreamReceiver> video_receiver_;
+    SimpleClient* other_ = nullptr;
 };
 
 int main()
 {
+    rtc::scoped_refptr<SimpleClient> sender = new rtc::RefCountedObject<SimpleClient>();
+    rtc::scoped_refptr<SimpleClient> receiver = new rtc::RefCountedObject<SimpleClient>();
+
     auto video_souce = new rtc::RefCountedObject<VideoTrack>();
-
     auto video_track = g_peer_connection_factory->CreateVideoTrack("video", video_souce);
+    auto peer_connection1 = get_default_peer_connection(g_peer_connection_factory, g_signal_thread.get(), sender.get());
+    peer_connection1->AddTrack(video_track, {"stream1"});
 
-    auto peer_connection = get_default_peer_connection(g_peer_connection_factory, g_signal_thread.get(), nullptr);
+    auto peer_connection2 = get_default_peer_connection(g_peer_connection_factory, g_signal_thread.get(), receiver.get());
 
-    auto err = peer_connection->AddTrack(video_track, {"stream1"});
+    sender->bind_peerconnection(peer_connection1);
+    receiver->bind_peerconnection(peer_connection2);
 
-    assert(err.ok());
+    peer_connection1 = nullptr;  //decrease ref
+    peer_connection2 = nullptr;  //decrease ref
+
+    sender->start();
+
+    std::string console;
+    while(console != "exit") {
+        std::cin >> console;
+    }
+
+    g_signal_thread->Invoke<void>(RTC_FROM_HERE, [&sender, &receiver](){
+        sender = nullptr;
+        receiver = nullptr;
+    });
+    g_signal_thread->Stop();
 
     return 0;
 }
